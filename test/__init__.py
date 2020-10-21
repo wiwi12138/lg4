fb1e537c52beec796b387a06d7ce55229cde3977
wiwi12138 <512118335@qq.com>
2020-10-21 17:52:31 +0800

    第一次commit

---
+++ test/__init__.py
@@ -0,0 +1,4 @@
+# -*— coding:utf-8 —*—
+# @time ：2020/10/2117:44
+# @Auttor :wiwi
+# @File :__init__.py.py
---
+++ test/test.py
@@ -0,0 +1,6 @@
+# -*— coding:utf-8 —*—
+# @time ：2020/10/2117:45
+# @Auttor :wiwi
+# @File :test.py
+
+print("hello world ")
