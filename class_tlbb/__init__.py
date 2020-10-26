9a9b73735bcc7cdff5a3bb3863132ba52b405c9a
wiwi12138 <512118335@qq.com>
2020-10-26 18:37:54 +0800

    第二次作业

---
+++ class_tlbb/__init__.py
@@ -0,0 +1,4 @@
+# -*— coding:utf-8 —*—
+# @time ：2020/10/2616:44
+# @Auttor :wiwi
+# @File :__init__.py.py
---
+++ class_tlbb/class_tstl.py
@@ -0,0 +1,4 @@
+# -*— coding:utf-8 —*—
+# @time ：2020/10/2618:37
+# @Auttor :wiwi
+# @File :class_tstl.py
