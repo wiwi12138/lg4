c987feef965a88ca4fe89a8a3320c0b035a76a21
wiwi12138 <512118335@qq.com>
2020-10-28 18:44:53 +0800

    第二次作业 class 类

--- class_tlbb/class_tstl.py
+++ class_tlbb/class_tstl.py
@@ -3,7 +3,6 @@
 # @Auttor :wiwi
 # @File :class_tstl.py
 
-
 class TongLao():
     def __init__(self,t_hp,t_power):
         self.t_hp=t_hp
@@ -18,11 +17,15 @@ class TongLao():
     def fight_zms(self):
         her_hp=2000
         her_power=200
-        her_hp=her_hp-t_power*10
-        t_hp=t_hp/2-her_power
+        her_hp=her_hp-self.t_power*10
+        t_hp=self.t_hp/2-her_power
         if t_hp>her_hp:
             print("我天山童姥赢了，哈哈。。")
         else:
             print("敌人太强了，我输了")
 
 
+if __name__ == "__main__":
+    tonglao=TongLao(2100,100)
+    tonglao.see_people('WYZ')
+    tonglao.fight_zms()
--- class_tlbb/xuzhu.py
+++ class_tlbb/xuzhu.py
@@ -1,14 +1,23 @@
 from class_tlbb.class_tstl import TongLao
+import random
 class XuZhu(TongLao):
 
-
+    def __init__(self,t_hp,t_power):
+        self.t_hp=t_hp
+        self.t_power=t_power
     def read(self):
 
-        print("罪过罪过")
-XuZhu()
-read()
+        print("虚竹：罪过罪过")
 
 if __name__ == "__main__":
-    tonglao = class_tstl.TongLao(1200, 100)
-    XuZhu()
-    read()
+    # tonglao = class_tstl.TongLao(1200, 100)
+    a=random.randint(1500,2000)
+    b=random.randint(100,200)
+    xuzhu=XuZhu(a,b)   # 实例化传参
+    print(f"我童姥的HP是{a}，攻击力是{b}")
+    xuzhu.see_people("丁春秋")
+    # xuzhu.see_people("李秋水")
+    xuzhu.fight_zms()
+    xuzhu.read()  # 调用read 函数
+
+
